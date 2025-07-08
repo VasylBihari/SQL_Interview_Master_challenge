3410e27016fbd0a07c8c4847711fbc9e346d4b6e
VasylBihari <vasylbihari@gmail.com>
2025-07-08 22:46:54 +0300

    Update Day_10.sql

--- Day_10.sql
+++ Day_10.sql
@@ -1,1 +1,12 @@
+/*Which AR filters have generated user interactions in July 2024? List the filters by name.
+Tables
+ar_filter_engagements(engagement_id, filter_id, interaction_count, engagement_date)
+ar_filters(filter_id, filter_name)*/
 
+SELECT
+  DISTINCT(filter_name)
+FROM ar_filters a
+INNER JOIN ar_filter_engagements e ON a.filter_id=e.filter_id
+WHERE e.engagement_date >= '2024-07-01' AND e.engagement_date <= '2024-07-31'
+
+/**/
